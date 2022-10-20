b12c3ea8322b6d8b6a9026c25e006cd4846ac655
sohyun03 <sohun1103@naver.com>
2022-10-20 15:47:04 +0900

    source

---
+++ src/main/java/com/joongbu/movie_reservation/JbGraduateApplication.java
@@ -0,0 +1,13 @@
+package com.joongbu.movie_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbGraduateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbGraduateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joongbu/movie_reservation/JbGraduateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongbu.movie_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbGraduateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
