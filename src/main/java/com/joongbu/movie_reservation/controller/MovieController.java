c0ee33a39655d8d0dba5c7e93e837f2637fa253f
sohyun03 <sohun1103@naver.com>
2022-10-24 16:38:52 +0900

    페이지 프론트 수정

---
+++ src/main/java/com/joongbu/movie_reservation/controller/MovieController.java
@@ -0,0 +1,13 @@
+package com.joongbu.movie_reservation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MovieController {
+	
+	@GetMapping("/movie.do")
+	public void rInser() {
+
+	}
+}
---
+++ src/main/java/com/joongbu/movie_reservation/controller/ReservationController.java
@@ -0,0 +1,25 @@
+package com.joongbu.movie_reservation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/reservation")
+@Controller
+public class ReservationController {
+
+	@GetMapping("/reserve.do")
+	public void rInser() {
+
+	}
+
+	@GetMapping("/seat.do")
+	public void h() {
+
+	}
+
+	@GetMapping("/payment.do")
+	public void a() {
+
+	}
+}
